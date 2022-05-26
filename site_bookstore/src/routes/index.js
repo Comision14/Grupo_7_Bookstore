1bc06da58af53876aac0ac9b74180a75d1ff0eb0
EricM76 <menaericdaniel@gmail.com>
2022-05-26 19:00:32 -0300

    update sprint 4

---
+++ site_bookstore/src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const {index, search}=require('../controllers/indexController')
+
+/* GET home page. */
+router.get('/',index)
+router.get('/search', search)
+
+module.exports = router;
